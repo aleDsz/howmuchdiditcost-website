862618634f1f9d39b8dff8eedc9747dce5189a66
Alexandre de Souza <alexandre@aledsz.com.br>
2016-08-23 03:32:50 -0300

    Update propuses section Update colors Update CSS classes Create menu on scroll page Update 'following menu' style

--- src/app/configs.js
+++ src/app/configs.js
@@ -11,6 +11,18 @@ $(document).ready(function() {
 	
 	$('.ui.sidebar').sidebar('attach events', '.toc.item');
 
+	$('.scrolly').click(function (e) {
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 1300);
+	});
+
+	$('.sidebar > .scrolly').click(function(e) {
+		e.preventDefault();
+		$('.ui.sidebar').sidebar('toggle');
+	});
+
 	/* $('#enviarEmail').click(function() {
 		$('#enviarEmail').addClass('loading');
 	}); */
